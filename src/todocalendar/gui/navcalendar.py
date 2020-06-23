f92262dfa4143b0156044408986084383d036f48
anetczuk <anetczuk@o2.pl>
2020-06-24 00:30:33 +0200

    Add context menu to NavCalendar

--- src/todocalendar/gui/navcalendar.py
+++ src/todocalendar/gui/navcalendar.py
@@ -23,11 +23,16 @@
 
 from PyQt5.QtWidgets import QCalendarWidget
 
+from PyQt5.QtCore import Qt
 from PyQt5.QtCore import QDate
 from PyQt5.QtGui import QColor
 from PyQt5.QtGui import QPalette
+from PyQt5.QtWidgets import QTableView
+from PyQt5.QtCore import pyqtSignal
+from PyQt5.QtWidgets import QMenu
 
 import abc
+import datetime
 
 
 class NavCalendarHighlightModel():
@@ -41,8 +46,14 @@ class NavCalendarHighlightModel():
 
 class NavCalendar( QCalendarWidget ):
 
+    addTask  = pyqtSignal( QDate )
+    addEvent = pyqtSignal( QDate )
+
     def __init__( self, *args ):
         QCalendarWidget.__init__( self, *args )
+
+        self.cellsTable = self.findChild( QTableView )
+
         self.taskColor = QColor( self.palette().color( QPalette.Highlight) )
         self.taskColor.setAlpha( 64 )
         self.highlightModel = None
@@ -73,3 +84,47 @@ class NavCalendar( QCalendarWidget ):
         if self.highlightModel is None:
             return False
         return self.highlightModel.isHighlighted( date )
+
+    def contextMenuEvent( self, event ):
+        evPos     = event.pos()
+        globalPos = self.mapToGlobal( evPos )
+        tabPos    = self.cellsTable.mapFromGlobal( globalPos )
+        cellIndex = self.cellsTable.indexAt( tabPos )
+        if cellIndex.row() < 1:
+            ## skip row with days of week
+            return
+        if cellIndex.column() < 1:
+            ## skip column with number of week
+            return
+
+        contextMenu = QMenu(self)
+        addTaskAction  = contextMenu.addAction("New Task")
+        addEventAction = contextMenu.addAction("New Event")
+        action = contextMenu.exec_( globalPos )
+        
+        if action == addTaskAction:
+            dayIndex = (cellIndex.row() - 1) * 7 + (cellIndex.column() - 1)
+            contextDate = self.dateAt( dayIndex )
+            self.addTask.emit( contextDate )
+        elif action == addEventAction:
+            dayIndex = (cellIndex.row() - 1) * 7 + (cellIndex.column() - 1)
+            contextDate = self.dateAt( dayIndex )
+            self.addEvent.emit( contextDate )
+
+    def dateAt( self, dayIndex ):
+        prevMonthDays = self.daysFromPreviousMonth()
+        dayOffset = dayIndex - prevMonthDays
+        currYear  = self.yearShown()
+        currMonth = self.monthShown()
+        currDate  = QDate( currYear, currMonth, 1 )
+        return currDate.addDays( dayOffset )
+
+    def daysFromPreviousMonth( self ):
+        currYear     = self.yearShown()
+        currMonth    = self.monthShown()
+        firstOfMonth = datetime.date( currYear, currMonth, 1 )
+        days = firstOfMonth.weekday()
+        if days == 0:                       # 0 means Monday
+            days += 7                       # there is always one row
+        return days
+
