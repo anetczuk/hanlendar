1a2ba6a66fceeeb7a1e887835c26489ab1d6707b
anetczuk <anetczuk@o2.pl>
2020-06-25 23:29:37 +0200

    Comment out unused code

--- src/todocalendar/domainmodel/event.py
+++ src/todocalendar/domainmodel/event.py
@@ -1,48 +1,48 @@
-# MIT License
+# # MIT License
+# #
+# # Copyright (c) 2020 Arkadiusz Netczuk <dev.arnet@gmail.com>
+# #
+# # Permission is hereby granted, free of charge, to any person obtaining a copy
+# # of this software and associated documentation files (the "Software"), to deal
+# # in the Software without restriction, including without limitation the rights
+# # to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# # copies of the Software, and to permit persons to whom the Software is
+# # furnished to do so, subject to the following conditions:
+# #
+# # The above copyright notice and this permission notice shall be included in all
+# # copies or substantial portions of the Software.
+# #
+# # THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# # IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# # FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# # AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# # LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# # OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# # SOFTWARE.
+# #
 #
-# Copyright (c) 2020 Arkadiusz Netczuk <dev.arnet@gmail.com>
+# from datetime import datetime, date, time
 #
-# Permission is hereby granted, free of charge, to any person obtaining a copy
-# of this software and associated documentation files (the "Software"), to deal
-# in the Software without restriction, including without limitation the rights
-# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-# copies of the Software, and to permit persons to whom the Software is
-# furnished to do so, subject to the following conditions:
 #
-# The above copyright notice and this permission notice shall be included in all
-# copies or substantial portions of the Software.
+# class Event():
+#     """Event is entity that happens instantly in moment of time."""
 #
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
-# SOFTWARE.
+#     def __init__(self):
+#         self.title               = ""
+#         self.description         = ""
+#         self.completed           = 0        ## in range [0..100]
+#         self.startDate: datetime = None
+#         self.reminder            = None
+#         self.recurrence          = None
 #
-
-from datetime import datetime, date, time
-
-
-class Event():
-    """Event is entity that happens instantly in moment of time."""
-
-    def __init__(self):
-        self.title               = ""
-        self.description         = ""
-        self.completed           = 0        ## in range [0..100]
-        self.startDate: datetime = None
-        self.reminder            = None
-        self.recurrence          = None
-
-    def setDefaultDateTime(self, start: datetime ):
-        self.startDate = start
-
-    def setDefaultDate(self, startDate: date):
-        start = datetime.combine( startDate, time(10, 0, 0) )
-        self.setDefaultDateTime( start )
-
-    def __str__(self):
-        return "[t:%s d:%s c:%s sd:%s rem:%s rec:%s]" % ( self.title, self.description, self.completed,
-                                                          self.startDate,
-                                                          self.reminder, self.recurrence )
+#     def setDefaultDateTime(self, start: datetime ):
+#         self.startDate = start
+#
+#     def setDefaultDate(self, startDate: date):
+#         start = datetime.combine( startDate, time(10, 0, 0) )
+#         self.setDefaultDateTime( start )
+#
+#     def __str__(self):
+#         return "[t:%s d:%s c:%s sd:%s rem:%s rec:%s]" % ( self.title, self.description, self.completed,
+#                                                           self.startDate,
+#                                                           self.reminder, self.recurrence )
