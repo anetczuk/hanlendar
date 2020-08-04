a7baeaf4702029de2176737e973fd8c3bc62a7e4
anetczuk <anetczuk@o2.pl>
2020-08-04 20:47:00 +0200

    Move files to sub folder

--- src/testhanlendar/gui/widget/__init__.py
+++ src/testhanlendar/gui/widget/__init__.py
@@ -25,4 +25,4 @@ import sys
 import os
 
 #### append source root
-sys.path.append(os.path.abspath( os.path.join(os.path.dirname(__file__), "..", "..") ))
+sys.path.append(os.path.abspath( os.path.join(os.path.dirname(__file__), "..", "..", "..") ))
