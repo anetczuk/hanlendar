f6a44a72f03c10c7142fb1a54fddbfed551a3486
anetczuk <anetczuk@o2.pl>
2022-05-29 00:14:14 +0200

    Add caldav client and server simple example

---
+++ example/caldav/startradicale.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+set -eu
+
+
+## works both under bash and sh
+SCRIPT_DIR=$(dirname "$(readlink -f "$0")")
+
+
+base_dir=$SCRIPT_DIR/../../tmp/radicale/collections
+config_file=$SCRIPT_DIR/radicale-config
+
+
+echo "Server dir: $base_dir"
+echo "Server config: $config_file"
+
+echo "Starting server. Available at: http://localhost:5232/"
+
+python3 -m radicale --storage-filesystem-folder=$base_dir --config=$config_file
+
